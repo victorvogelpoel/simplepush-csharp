59f7e6d1900a473133138107abe52ba9613d9c35
Victor Vogelpoel <victor@victorvogelpoel.nl>
2021-09-30 17:53:52 +0200

    Add c# version using simplepush notification service

---
+++ src-net48/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace simplepush_csharp_net48
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using (var client = new HttpClient())
+            {
+                string serviceUrl = "https://api.simplepush.io/send";
+
+                var data = new Dictionary<string, string>
+                {
+                    { "key",    "HuxgBB" },
+                    { "title",  "title" },
+                    { "msg",    "message" },
+                    { "event",  "event" }
+                };
+
+
+                // -------------------------------------------------------------------------------
+                // using regular HttpClient and Json.NET for serializing the data
+                data["msg"] = "message using regular HttpClient";
+                var json = JsonConvert.SerializeObject(data);
+                var postdata = new StringContent(json, Encoding.UTF8, "application/json");
+                var response = await client.PostAsync(serviceUrl, postdata);
+                response.EnsureSuccessStatusCode();
+
+                // -------------------------------------------------------------------------------
+                // Another way using System.Net.Http.Json
+                data["msg"] = "message using System.Net.Http.Json PostAsJsonAsync";
+                response = await client.PostAsJsonAsync(serviceUrl, data);
+                response.EnsureSuccessStatusCode();
+            }
+        }
+    }
+}
